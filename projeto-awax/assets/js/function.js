adbb35eed8766c7d79d98689604216d8c6c89b6a
Renan Reis <renanreis.dev@gmail.com>
2021-04-18 18:49:33 -0300

    Adicionado o efeito do menu tablets e mobile com JS

---
+++ projeto-awax/assets/js/function.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    document.querySelector(".menu-opener").addEventListener("click", function(){
+        if (document.querySelector("nav").style.display == "flex") {
+            document.querySelector("nav").style.display = "none";
+        } else {
+            document.querySelector("nav").style.display = "flex";
+        }
+    });
+
+}
